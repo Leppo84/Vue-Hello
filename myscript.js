6a86deaf7a0477077e229d0794cab5a4fdb0cdfc
LeppoXena <francesco.lepore@xenagames.com>
2022-05-24 19:59:01 +0200

    Start and end

---
+++ myscript.js
@@ -0,0 +1,14 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data (va bene qualsiasi immagine che prendete dal web e/o avete e/o vi scaricate, è indifferente)
+
+const stopWar = new Vue({
+    el: "#splash",
+    data: {
+        image:"https://media.istockphoto.com/photos/map-of-green-ukraine-on-a-globe-picture-id163113558?k=20&m=163113558&s=612x612&w=0&h=i1w6ELou3CNNdfHEGqWQPgHu4n1e9pHvR39EpkENUtY=",
+        classSplash:"splash-img",
+        classTitle:"title",
+        yell:"STOP WAR",
+    }
+})
+
+// this.image = "'some/url/path/to/image.jpeg'"
